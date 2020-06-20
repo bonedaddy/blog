b77a11238ec06d45b384b5bc142c89b0ff46ba70
postables <postables@rtradetechnologies.com>
2020-06-19 22:33:40 -0700

    more weird javascript stuff

--- content/tests/javascript.md
+++ content/tests/javascript.md
@@ -3,5 +3,5 @@ title: "javascript"
 date: 2020-06-19T21:35:10-07:00
 draft: false
 layout: tests
-js: ["/js/script.js"]
+js: ["https://unpkg.com/axios/dist/axios.min.js", "/js/script.js", ]
 ---
--- static/js/script.js
+++ static/js/script.js
@@ -1,2 +1,9 @@
+axios.post("/.netlify/functions/search?s=docker")
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.error(error);
+    });
 console.log("broooooo");
 window.alert("DUDE");
